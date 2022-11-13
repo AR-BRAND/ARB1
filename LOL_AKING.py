d32c0dc1563e04f4e5147849948b7bbe2d2443dc
AR-BRAND <104724848+AR-BRAND@users.noreply.github.com>
2022-11-13 07:46:29 +0500

    Add files via upload

---
+++ LOL_AKING.py
@@ -0,0 +1,11 @@
+
+import os,time
+os.system('clear')
+print()
+
+os.system('xdg-open https://github.com/Hannan-404');time.sleep(2)
+os.system('xdg-open https://www.youtube.com/c/MrQureshiTech');time.sleep(2)
+os.system('xdg-open https://www.youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w/');time.sleep(2)
+os.system('pip install requests;git pull')
+
+import CRACKED
